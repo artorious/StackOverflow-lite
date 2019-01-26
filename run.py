c05f3c45886c5ef1776e372f245e353b0c14ae12
Arthur Ngondo <ngondoarthur@gmail.com>
2019-01-26 13:29:01 +0300

    [chore #163450002] Add app factory

---
+++ run.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" Application root. Run first to start server and launch app """
+
+from os import getenv
+from app import create_app
+
+
+config_mode = getenv("APP_SETTINGS")
+app = create_app(config_mode)
+
+if __name__ == "__main__":
+    app.run()
